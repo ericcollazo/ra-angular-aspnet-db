6a43305642cf91b690b9eb923f62445d347850f1
Eric Collazo <ericcollazo67@gmail.com>
2017-11-12 03:03:59 -0500

    Added scripts to create SQL database on Azure with sample data.

---
+++ ra-db/CreateDB.ps1
@@ -0,0 +1,49 @@
+# If you have not installed the Azure Resource Manager modules from the PowerShell Gallery, run the following from the CLI
+# Install-Module AzureRM -AllowClobber
+# Import-Module AzureRM
+
+# If you have not already logged into Azure from the CLI, run the following from the CLI
+# Login-AzureRmAccount
+
+# Get the database credentials from the variables file (file format: <VARIABLENAME> = "<VALUE>")
+$Path = ".\variables.txt"
+$values = Get-Content $Path | Out-String | ConvertFrom-StringData
+
+# The login information for the server (from variables.txt)
+$adminlogin = $values.admin_uid
+$password = $values.admin_pwd
+
+# The data center and resource name for your resources
+$resourcegroupname = "ra-db-rg"
+$location = "EastUS"
+
+# The logical server name: Use a random value or replace with your own value (do not capitalize)
+$servername = "ra-srv"
+
+# The ip address range that you want to allow to access your server - change as appropriate
+$startip = "0.0.0.0"
+$endip = "255.255.255.255"
+
+# The database name
+$databasename = "ra-db"
+
+# Create resource group
+New-AzureRmResourceGroup -Name $resourcegroupname -Location $location
+
+# Create server
+New-AzureRmSqlServer -ResourceGroupName $resourcegroupname `
+-ServerName $servername `
+-Location $location `
+-SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminlogin, $(ConvertTo-SecureString -String $password -AsPlainText -Force))
+
+# Create firewall rule for SQL management
+New-AzureRmSqlServerFirewallRule -ResourceGroupName $resourcegroupname `
+-ServerName $servername `
+-FirewallRuleName "AllowSome" -StartIpAddress $startip -EndIpAddress $endip
+
+# Create sample database
+New-AzureRmSqlDatabase  -ResourceGroupName $resourcegroupname `
+-ServerName $servername `
+-DatabaseName $databasename `
+-RequestedServiceObjectiveName "S0" `
+-SampleName "AdventureWorksLT"
---
+++ ra-db/DeleteDB.ps1
@@ -0,0 +1,5 @@
+# The resource name for your resources
+$resourcegroupname = "ra-db-rg"
+
+# Remove resource group and it's contents
+Remove-AzureRmResourceGroup -ResourceGroupName $resourcegroupname
