d068ee087a5f9dd2465c02db1c2abecb7713d338
Eric Collazo <ericcollazo67@gmail.com>
2017-11-30 15:50:05 -0500

    Added EF scaffolding code and objects.

---
+++ ra-api/Models/BuildVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ra-api.Models
+{
+    public partial class BuildVersion
+    {
+        public byte SystemInformationId { get; set; }
+        public string DatabaseVersion { get; set; }
+        public DateTime VersionDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
