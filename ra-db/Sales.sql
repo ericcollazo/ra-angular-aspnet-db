c67d738586c0c0824f4fc7292c91979d6b6db0ef
Eric Collazo <ericcollazo67@gmail.com>
2017-12-28 13:22:30 -0500

    Removed superfluous files.

--- ra-db/Sales.sql
+++
@@ -1,8 +0,0 @@
-select
-    sum(Header.TotalDue), OrderProduct.Name
-from ((SalesLT.SalesOrderHeader as Header
-    inner join SalesLT.SalesOrderDetail as Detail on Header.SalesOrderID = Detail.SalesOrderID)
-    inner join SalesLT.Product as OrderProduct on Detail.ProductID = OrderProduct.ProductID)
-group by OrderProduct.Name
-order by sum(Header.TotalDue) DESC
-GO
